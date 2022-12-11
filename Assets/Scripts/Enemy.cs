d0450818216096ae5e1d5240b6927bcd3da59dc2
PabloVinii <pvinicius006@hotmail.com>
2022-12-11 14:29:39 -0300

    criado GameManager

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float enemyVelocity;
+    public Transform[] pointsToWalk;
+    private int currentpoint;
+    public bool enemyAlive;
+    public bool enemyCanWalk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
